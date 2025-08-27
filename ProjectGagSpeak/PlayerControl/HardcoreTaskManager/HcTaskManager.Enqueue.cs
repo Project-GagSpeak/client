f7f4a62518ecd9609c56b1268bfb6a59e4efa9ba
Cordelia Mist <buzzmodetrix@gmail.com>
2025-08-26 18:53:01 -0700

    fixed some duplicate code

--- ProjectGagSpeak/PlayerControl/HardcoreTaskManager/HcTaskManager.Enqueue.cs
+++ ProjectGagSpeak/PlayerControl/HardcoreTaskManager/HcTaskManager.Enqueue.cs
@@ -20,24 +20,6 @@ public partial class HcTaskManager
     public void EnqueueTask(Action action, HcTaskConfiguration? config = null)
         => EnqueueOperation(new HardcoreTask(action, config ?? HcTaskConfiguration.Default));
 
-    public void EnqueueTask(Func<bool?> func, string name, HcTaskConfiguration? config = null)
-        => EnqueueOperation(new HardcoreTask(func, name, config ?? HcTaskConfiguration.Default));
-
-    public void EnqueueTask(Func<bool?> func, HcTaskConfiguration? config = null)
-        => EnqueueOperation(new HardcoreTask(func, config ?? HcTaskConfiguration.Default));
-
-    public void EnqueueTask(Func<bool> func, string name, HcTaskConfiguration? config = null)
-        => EnqueueOperation(new HardcoreTask(func, name, config ?? HcTaskConfiguration.Default));
-
-    public void EnqueueTask(Func<bool> func, HcTaskConfiguration? config = null)
-        => EnqueueOperation(new HardcoreTask(func, config ?? HcTaskConfiguration.Default));
-
-    public void EnqueueTask(Action action, string name, HcTaskConfiguration? config = null)
-        => EnqueueOperation(new HardcoreTask(action, name, config ?? HcTaskConfiguration.Default));
-
-    public void EnqueueTask(Action action, HcTaskConfiguration? config = null)
-        => EnqueueOperation(new HardcoreTask(action, config ?? HcTaskConfiguration.Default));
-
     public void EnqueueOperation(HardcoreTaskBase task)
     {
         if (task is null)
