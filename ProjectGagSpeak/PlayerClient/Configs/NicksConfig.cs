e73333151bad86402af00a87a782a8d1bc868361
Andriel Chaoti <3628387+AndrielChaoti@users.noreply.github.com>
2026-05-28 12:19:21 -0600

    Add functionality to clear a user's nickname.

--- ProjectGagSpeak/PlayerClient/Configs/NicksConfig.cs
+++ ProjectGagSpeak/PlayerClient/Configs/NicksConfig.cs
@@ -86,7 +86,7 @@ public class NicksConfig : IHybridSavable
         => Current.Nicknames.TryGetValue(uid, out var n) && n is { Length: > 0 } ? n : null;
 
 
-    /// <summary> 
+    /// <summary>
     ///     Set a nickname for a user identifier.
     /// </summary>
     /// <param name="uid">the user identifier</param>
@@ -97,6 +97,15 @@ public class NicksConfig : IHybridSavable
             return;
 
         Current.Nicknames[uid] = nickname;
+        Save();
+    }
+
+    public void ClearNickname(string userDataUID)
+    {
+        if (userDataUID.IsNullOrWhitespace())
+            throw new ArgumentException("UserDataUID cannot be null or empty.");
+
+        Current.Nicknames.Remove(userDataUID);
         Save();
     }
 }
