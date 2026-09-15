cb7fc3957e1f4188504a34767b6e158281ffa8d7
Cordelia Mist <buzzmodetrix@gmail.com>
2026-09-15 10:58:09 -0700

    Settings Overhaul and import remaining part of the update 2.2.2.0

--- ProjectGagSpeak/WebAPI/Hubs/MainHub.Connection.cs
+++ ProjectGagSpeak/WebAPI/Hubs/MainHub.Connection.cs
@@ -1,6 +1,8 @@
 using CkCommons;
+using Dalamud.Interface.ImGuiNotification;
 using GagSpeak.PlayerClient;
 using GagSpeak.Services.Mediator;
+using GagspeakAPI.Hub;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.AspNetCore.SignalR.Client;
 using System.Net.WebSockets;
